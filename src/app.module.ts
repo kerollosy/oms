677297877eb70e17c1e9b757ce86aa63d8aba0e5
Omak2r3a <86030102+kerollosy@users.noreply.github.com>
2024-06-23 02:48:15 +0300

    Add cart module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { CartModule } from './cart/cart.module';
 import { UsersModule } from './users/users.module';
 import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [CartModule, UsersModule],
   controllers: [],
   providers: [PrismaService],
 })
